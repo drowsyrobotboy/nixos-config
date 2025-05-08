0ebff6daf383becf3aec0dcadc6622b828eeb7ca
Maruthi Sharma <maruthisharma@live.in>
2025-05-08 09:37:07 +0530

    Create home.nix

---
+++ home.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, lib, ... }:
+
+{
+    home.stateVersion = "24.11";
+    home.packages = [ pkgs.git ];
+
+    # Use full path of git binary so that command not found issue doesn't arise
+    home.activation.cloneRepo = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      mkdir -p "$HOME/Projects"
+      if [ ! -d "$HOME/Projects/sinma" ]; then
+        ${pkgs.git}/bin/git clone https://github.com/drowsyrobotboy/sinma.git "$HOME/Projects/sinma"
+      fi
+    '';
+}
