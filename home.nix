6f4a5b8516eb80cdb4a8c91b99e351af5eb2ba24
Maruthi Sharma <maruthisharma@live.in>
2025-05-12 00:47:02 +0530

    Update home.nix for numlock :')

--- home.nix
+++ home.nix
@@ -4,6 +4,9 @@
     home.stateVersion = "24.11";
     home.packages = [ pkgs.git ];
 
+    # Enable numLock
+    xsession.numlock.enable = true;
+
     # Use full path of git binary so that command not found issue doesn't arise
     home.activation.cloneRepo = lib.hm.dag.entryAfter ["writeBoundary"] ''
       mkdir -p "$HOME/Projects"
